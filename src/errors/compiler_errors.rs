f74b30ba86a000eea3de8ea72bbd405a38fc484e
Dhruv Jimulia <dj321@ic.ac.uk>
2023-08-01 23:07:41 +0100

    Refactor main function and improve compiler errors

--- src/errors/compiler_errors.rs
+++ src/errors/compiler_errors.rs
@@ -7,10 +7,10 @@ pub const UNFINISHED_THOUGHT_ERROR: Error = Error {
 
 pub const FILE_NOT_FOUND_ERROR: Error = Error {
     error_type: "Plot Not Found Error",
-    error_message: "In the vast library of tales, the chapter you seek is lost in the unfathomable void. Perhaps it slipped through the cracks of existence or became entangled in the quantum flux. Seek it elsewhere, brave explorer, for it eludes us here."
+    error_message: "In the vast library of tales, you rummage through the bookshelves but fail to find the chapter you seek. Perhaps it slipped through the cracks of existence or became entangled in the quantum flux. Seek it elsewhere, fearless adventurer, for it eludes us here."
 };
 
 pub const ILLEGAL_ARGUMENT_ERROR: Error = Error {
     error_type: "Enigmatic Whispers Error",
-    error_message: "Listen closely, brave programmer, for the cryptic whispers of your command-line summons are incomprehensible even to the wise compiler. Alas, the words you offer are but echoes of disarray, and they remain elusive. Invoke your commands with precision to unravel the mysteries your story can offer."
+    error_message: "Listen closely, brave programmer, for the cryptic whispers of your command-line summons are incomprehensible even to the wise compiler. Alas, the words you offer are but echoes of disarray. Invoke your commands with precision to unravel the mysteries your story can offer."
 };
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use storyteller::errors::compiler_errors;
 
 fn main() {
-    let file_name = match std::env::args().nth(1) {
-        Some(file_name) => file_name,
-        None => {
-            compiler_errors::ILLEGAL_ARGUMENT_ERROR.display();
-            return;
-        }
-    };
-    storyteller::interpret(file_name,  &mut std::io::stdin().lock(), &mut std::io::stdout());
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() != 2 {
+        compiler_errors::ILLEGAL_ARGUMENT_ERROR.display();
+    }
+    let file_name = &args[1];
+    storyteller::interpret(file_name.clone(),  &mut std::io::stdin().lock(), &mut std::io::stdout());
 }
