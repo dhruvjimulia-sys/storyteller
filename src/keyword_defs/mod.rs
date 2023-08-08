92ff94915f53f9966af695c3456020d0878042b3
Dhruv Jimulia <dj321@ic.ac.uk>
2023-08-08 20:56:18 +0100

    Begin refactor to keywords in txt file

--- src/keyword_defs/mod.rs
+++ src/keyword_defs/mod.rs
@@ -1,4 +1,6 @@
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 pub struct KeywordDefs {
     pub to_be: HashSet<String>,
@@ -10,12 +12,25 @@ pub struct KeywordDefs {
     pub negative_comparative_adjective: HashSet<String>
 }
 
+fn create_string_set_from_file(file_path: &str) -> HashSet<String> {
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut string_set = HashSet::new();
+
+    for line in reader.lines() {
+        string_set.insert(line.unwrap());
+    }
+
+    string_set
+}
+
 pub fn defs() -> KeywordDefs {
     fn to_strings(set: HashSet<&str>) -> HashSet<String> {
         set.into_iter().map(|s| s.to_string()).collect::<HashSet<_>>()
     }
     KeywordDefs {
-        to_be: to_strings(HashSet::from(["was", "were", "is", "are", "wanted to be like", "wants to be like", "wanted to be like"])),
+        to_be: create_string_set_from_file("keywords/to_be_keywords.txt"),
         positive_adjective: to_strings(HashSet::from(["good", "great", "awesome", "amazing", "fantastic", "wonderful", "incredible", "nice", "cool", "happy", "joyful", "joyous", "glad", "delighted", "pleased", "satisfied", "content", "cheerful", "merry", "jolly", "jovial", "gleeful", "carefree", "sunny", "elated", "exhilarated", "ecstatic", "euphoric", "overjoyed", "exultant", "rapturous", "blissful", "radiant", "thrilled", "ravished"])),
         negative_adjective: to_strings(HashSet::from(["bad", "terrible", "awful", "horrible", "dreadful", "unpleasant", "unlucky", "displeased", "miserable", "sad", "sorrowful", "dejected", "regretful", "depressed", "downcast", "despondent", "disconsolate", "desolate", "glum", "gloomy", "melancholic", "mournful", "forlorn", "crestfallen", "broken-hearted", "heartbroken", "grief-stricken", "disheartened", "dismayed", "dispirited", "discouraged", "hopeless"])),
         said: to_strings(HashSet::from(["said", "stated", "exclaimed", "whispered", "shouted", "mumbled", "replied", "responded", "declared", "announced", "asserted", "acknowledged", "conveyed", "uttered", "ventured", "suggested", "disclosed", "protested", "objected", "interjected", "speculated", "greeted", "quoted", "noted", "mentioned", "alledged", "insisted", "confessed", "recited", "pleaded", "concluded", "inquired", "muttered"])),
